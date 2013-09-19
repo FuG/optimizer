53f227720144b65b48b2faf50e61bcc6c2fd59f8
Gary Fu <gfu@marchex.com>
2013-09-19 16:10:06 -0700

    Added Bullet. Setup Weapon fields.

---
+++ src/main/Bullet.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gfu
+ * Date: 9/19/13
+ * Time: 3:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Bullet {
+    public float vertical_recoil_angle,
+                 horizontal_recoil_angle;
+    public float spread_angle;
+}
--- src/main/Weapon.java
+++ src/main/Weapon.java
@@ -8,16 +8,39 @@ package main;
  * To change this template use File | Settings | File Templates.
  */
 public class Weapon {
-    public static enum FiringMode {
-        SINGLE,
-        BURST,
-        AUTO;
+    // Damage
+    public float damage_max,
+                 damage_min;
+    public float damage_drop_start,
+                 damage_drop_stop;
+
+    // Spread
+    public float spread_base;
+    public float spread_increase,
+                 spread_decrease;
+
+    // Recoil
+    public float recoil_up,
+                 recoil_left,
+                 recoil_right;
+    public float recoil_decrease;
+    public float recoil_first_shot_mult;
+
+    // Misc.
+    public int rounds_per_minute;
+
+    // Tracking
+    public float current_spread;
+    public float current_recoil_vertical,
+                 current_recoil_horizontal;
+    public int current_bullet_count;
+
+
+    public Bullet fire() {
+        return null;
     }
 
-    public class Recoil {
-        public float up,
-                down,
-                left,
-                right;
+    public void release() {
+        current_bullet_count = 0;
     }
 }
